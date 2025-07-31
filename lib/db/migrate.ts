e36472334a2eca5e2ae0e0140057815543151e33
Jason <jason@hedmans.org>
2025-07-31 18:04:40 -0400

    rm development flag

--- lib/db/migrate.ts
+++ lib/db/migrate.ts
@@ -3,11 +3,9 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import { migrate } from 'drizzle-orm/postgres-js/migrator';
 import postgres from 'postgres';
 
-if (process.env.NODE_ENV === 'development') {
-  config({
-    path: '.env.local',
-  });
-}
+config({
+  path: '.env.local',
+});
 
 const runMigrate = async () => {
   if (!process.env.POSTGRES_URL) {
