3b3247bf5f5a16e5ca8bbb92675c930b9c6d13af
Jason <jason@hedmans.org>
2025-07-31 18:01:23 -0400

    update migration script

--- lib/db/migrate.ts
+++ lib/db/migrate.ts
@@ -3,9 +3,11 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import { migrate } from 'drizzle-orm/postgres-js/migrator';
 import postgres from 'postgres';
 
-config({
-  path: '.env.local',
-});
+if (process.env.NODE_ENV === 'development') {
+  config({
+    path: '.env.local',
+  });
+}
 
 const runMigrate = async () => {
   if (!process.env.POSTGRES_URL) {
